a6d797218bc7dbba94c5f021c4f540ccc42f0e17
Jonathan Chancey <jchancey@ucmerced.edu>
2019-03-02 01:35:00 -0800

    literally whitespace

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 
 import {Icon} from "semantic-ui-react";
 
+
+
 //IMPORT COMPONENTS
 import NavBar from "./components/navBar";
 import Map from "./components/map";
+
 
 class App extends Component {
   render() {
