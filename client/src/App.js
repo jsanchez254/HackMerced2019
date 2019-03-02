8c46fc5f78c3621eddfa4740c7f844f42a3d6bb3
jsanchez254 <jsanchez254@ucmerced.edu>
2019-03-01 23:19:20 -0800

    map works

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 
+import {Icon} from "semantic-ui-react";
+
 //IMPORT COMPONENTS
 import NavBar from "./components/navBar";
+import Map from "./components/map";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Icon name = "user"/>
         <h1>HERE WE GO</h1>
         <NavBar/>
+        <Map/>
       </React.Fragment>
     );
   }
