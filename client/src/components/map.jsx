d535502bd1fb707eff06b694258ef9144c44d736
jsanchez254 <jsanchez254@ucmerced.edu>
2019-03-02 00:25:53 -0800

    cool

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -1,33 +1,62 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
  
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => <h1 className = "title">hhhh</h1>;
  
 class SimpleMap extends Component {
-  static defaultProps = {
+  state = { 
+    userLocation: { lat: 70, lng: 32 }, 
+    loading: true,
+    zoom : 8,
     center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
+        lat: 0,
+        lng: 0
+    }
   };
- 
+
+
+  componentDidMount =(props) => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        const { lat, lng } = position.coords;
+        this.setState({
+            center: { lat: latitude, lng: longitude }
+        })
+        this.setState({
+          userLocation: { lat: latitude, lng: longitude },
+          loading: false
+        });
+      },
+      () => {
+        this.setState({ loading: false });
+      }
+    );
+  }
+
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyDKg5413ZLcyIicH6ybYlh_fFHSf1BnqQQ" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={'Kreyser Avrora'}
-          />
-        </GoogleMapReact>
-      </div>
+    <React.Fragment>
+        <h1>{this.state.userLocation.lat}</h1>
+        <h1>{this.state.userLocation.lng}</h1>
+        <h1>{this.state.center.lng1}</h1>
+        <h1>{this.state.center.lat1}</h1>
+        <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMapReact
+            bootstrapURLKeys={{ key: "AIzaSyDKg5413ZLcyIicH6ybYlh_fFHSf1BnqQQ" }}
+            defaultCenter={this.state.center}
+            defaultZoom={this.state.zoom}
+            >
+            {/* just for text and DOM stuff */}
+            <AnyReactComponent
+                lat={this.state.userLocation.lat}
+                lng={this.state.userLocation.lng}
+                text={this.state.userLocation.lat}
+            />
+            </GoogleMapReact>
+        </div>
+    </React.Fragment>
     );
   }
 }
